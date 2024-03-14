db860a1e86cec827640658af94771dafaf010836
Service Web <sw@gssmail.com>
2024-03-14 12:23:49 -0500

    Commit changes in C:\Websites\Git\emarlow\5559\GCG_5559_WORKCENTERS.sql

---
+++ GCG_5559_WORKCENTERS.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_5559_WORKCENTERS
+ * ========================================== */
+CREATE TABLE "GCG_5559_WORKCENTERS"(
+ "WORKCENTER" CHAR(4),
+ "PRIORITY" CHAR(3));
+
+

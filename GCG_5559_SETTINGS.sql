7c20a6f606969772b99eed422c32f8999da45d3f
Service Web <sw@gssmail.com>
2024-03-14 12:23:49 -0500

    Commit changes in C:\Websites\Git\emarlow\5559\GCG_5559_SETTINGS.sql

---
+++ GCG_5559_SETTINGS.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_5559_SETTINGS
+ * ========================================== */
+CREATE TABLE "GCG_5559_SETTINGS"(
+ "SETTING" CHAR(20),
+ "VALUE" LONGVARCHAR);
+
+
